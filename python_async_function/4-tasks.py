06c07b566165ae4459b0080794639edf8edc2854
xavierdev <xavvis308@gmail.com>
2024-08-15 10:27:06 -0400

    pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-This module take the code from wait_n and alter it into a new function task_wait_n. 
+This module take the code from wait_n and alter it into a new function
+task_wait_n.
 """
 
 import asyncio
