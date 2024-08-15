4eafc08703d6f875b84bba602e34b883db5d16ac
xavierdev <xavvis308@gmail.com>
2024-08-15 13:03:36 -0400

    Refactored code for better clarity

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python3
-"""
-This module take the code from wait_n and alter it into a new function
-task_wait_n.
-"""
+"""Module to manage tasks and delays using asyncio."""
 
 import asyncio
-from typing import List, Any
-
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ Identical to task 1 but task_wait_random is being called """
-    allDelays: List = []
-
-    for i in range(n):
-        allDelays.append(task_wait_random(max_delay))
-
-    List_of_tasks: List[Any] = []
-
-    for results in asyncio.as_completed(allDelays):
-        """ wait for as_completed to return """
-        completed: float = await results
-        List_of_tasks.append(completed)
-
-    return List_of_tasks
+    """Create and gather n tasks, return sorted delays."""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
