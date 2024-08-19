95f3ca7916536a2ce255b72c101df3e8e4546f6a
Xavier J. Cruz <xavvis308@gmail.com>
2024-08-19 17:36:04 -0400

    Update code_ThatDoNotPass_mypy.py

--- python_variable_annotations/code_ThatDoNotPass_mypy.py
+++ python_variable_annotations/code_ThatDoNotPass_mypy.py
@@ -1,6 +1,1 @@
-def add_numbers(a: int, b: int) -> int:
-    # The function is supposed to return an int, but it returns a string instead
-    return "The sum is: " + str(a + b)
 
-# This function call expects integer arguments, but strings are provided
-result = add_numbers("three", "five")
