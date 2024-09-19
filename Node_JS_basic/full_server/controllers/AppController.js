cdc1a1d22e2ad6b02cf14c65243311e315fa615f
xavierdev <xavvis308@gmail.com>
2024-09-19 19:15:27 -0400

    eslint

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,8 +1,7 @@
 class AppController {
-    static getHomepage(req, res) {
-      res.status(200).send('Hello Holberton School!');
-    }
+  static getHomepage(req, res) {
+    res.status(200).send('Hello Holberton School!');
   }
-  
-  module.exports = AppController;
-  
+}
+
+module.exports = AppController;
